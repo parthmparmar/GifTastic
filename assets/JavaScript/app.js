dfc2de1a9aa581887316a883c5faee2cb7ca3e2a
Parth Parmar <parthmparmar@gmail.com>
2019-10-11 22:03:51 -0400

    added ajax call to GIMPHY api to get links to gifs for selected button, used for loop to create 10 cards with gif links from the data provided by GIMPHY api. Added event on click to play and pause gif image.

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,8 +1,10 @@
 var button_array = ["cat", "dog", "bat", "spider", "rat", "lion", "tiger", "hawk", "parrot"];
 
 
-var btnSetup = $("#btn-setup");
+var btnSetup = $(".btn-secondary");
 var buttonDisplay = $(".button-display");
+var cardSetup = $(".card")
+var gifDisplay = $(".gif-display")
 
 function add_button(button_name, add_to){
     var newBtn = btnSetup.clone(true);
@@ -12,12 +14,33 @@ function add_button(button_name, add_to){
     add_to.append(newBtn);
 }
 
+function add_gif (stillLinkInput, playLinkInput, ratingInput, addTo) {
+    var newCard = cardSetup.clone(true);
+    newCard.attr("data-still", stillLinkInput);
+    newCard.attr("data-play", playLinkInput);
+    newCard.attr("data-status", "still");
+    newCard.removeClass("off");
+    newCard.children().attr("src", stillLinkInput);
+    newCard.find("#rating").text("Raiting: "+ ratingInput);
+    addTo.append(newCard)
+}
+
 for (var i = 0; i < button_array.length; i++) {
     add_button(button_array[i], buttonDisplay);
 }
 
-$(".btn").on("click", function (){
-    console.log($(this).attr("data-item"));
+$(".btn-secondary").on("click", function (){
+    var animalName = $(this).attr("data-item");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=7cDSLMzhZcERGBKhmFgrnnS0pg9sAEtR&q=" + animalName + "&limit=25&offset=0&lang=en";
+    $.ajax({url: queryURL, method: "GET"}).then(function(results){
+        console.log(results)
+        for (var i = 0; i < 11; i++){
+            var stillLink = results.data[i].images.fixed_width_still.url;
+            var playLink = results.data[i].images.fixed_width.url;
+            var rating = results.data[i].rating
+            add_gif(stillLink, playLink, rating, gifDisplay);
+        }
+    });
 });
 
 $("#add").on("click", function (){
@@ -25,4 +48,19 @@ $("#add").on("click", function (){
     if (add_name != "") {
         add_button(add_name, buttonDisplay);
     }
+});
+
+$(".card").on("click", function(){
+    var gifCard = $(this);
+    var status = gifCard.attr("data-status");
+    
+    if (status == "still"){
+        gifCard.children().attr("src", gifCard.attr("data-play"));
+        gifCard.attr("data-status", "play");
+    }
+    else if (status == "play"){
+        gifCard.children().attr("src", gifCard.attr("data-still"));
+        gifCard.attr("data-status", "still");
+    }
+
 });
